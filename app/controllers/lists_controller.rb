bf1cde98e773a32595f93562d461cebb48d38b46
EC2 Default User <175428025+tomita0731@users.noreply.github.com>
2024-09-05 15:11:38 +0000

    [Add] 3-4エラー改善

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -9,9 +9,11 @@ class ListsController < ApplicationController
     redirect_to '/top'
   end
   def index
+    @lists = List.all
   end
 
   def show
+    @list = List.find(params[:id])
   end
 
   def edit
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
+   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   get 'lists/new'
-  post 'lists' =>'lists#create'
-  get 'lists/index'
-  get 'lists/show'
   get 'lists/edit'
-  get '/top' => 'homes#top'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post 'lists' =>'lists#create'
+  get 'top' => 'homes#top'
+  get 'lists' => 'lists#index'
+　get 'lists/:id' => 'lists#show'
 end
