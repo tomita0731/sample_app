7365dd084e854993ca0f7eaa4c341c32dd94ccc3
EC2 Default User <175428025+tomita0731@users.noreply.github.com>
2024-09-08 10:33:44 +0000

    [finish] 3-5

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -6,8 +6,9 @@ class ListsController < ApplicationController
   def create
     list = List.new(list_params)
     list.save
-    redirect_to '/top'
+    redirect_to list_path(list.id)
   end
+  
   def index
     @lists = List.all
   end
@@ -17,7 +18,14 @@ class ListsController < ApplicationController
   end
 
   def edit
+    @list = List.find(params[:id])
   end
+  
+def update
+  list = List.find(params[:id])
+  list.update(list_params)
+  redirect_to list_path(list.id)
+end
 
 private
   def list_params
